a89768f3b7a0161f92a5e41f8d15507dab6bb93b
Dominic Gan <dominicgangx@gmail.com>
2016-03-06 22:47:31 +0800

    list hover animations, added lightbox functionality for posts.

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -5,8 +5,43 @@ var $postGrid = $('.post-grid').masonry({
 	percentPosition: true
 });
 
+function kickLinksOut(link){
+
+	if (!(link.is('a'))){
+		console.log(link + "is not a link.");
+		return false;
+	}
+
+	var linkArr = link.get();
+
+	for (i=0; i<linkArr.length; i++){
+		$(linkArr[i]).attr("target","_blank");
+	}
+}
+
+function initFeatherlight(){
+	var imgArr = $('article img').get();
+	var hrefVal;
+
+	for (i=0; i<imgArr.length; i++){
+		 hrefVal = $(imgArr[i]).attr('src');
+		 $(imgArr[i]).attr('data-featherlight',hrefVal);
+	}
+
+	$('article img').featherlight();
+
+}
+
 $(document).ready(function(){
 	$postGrid.imagesLoaded().progress( function() {
 		$postGrid.masonry('layout');
 	});
+
+	var $socialLinks = $('.social-media-list li a');
+
+	kickLinksOut($socialLinks);
+
+	if ($('article').length > 0) {
+		initFeatherlight();
+	}
 });
