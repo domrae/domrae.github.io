5acc259e8320f2a2e4a1ccf4c5601a56144f25ee
Dominic Gan <dominicgangx@gmail.com>
2018-04-06 01:02:56 +0800

    chore(gulp): remove unused lib declaration

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,7 +1,6 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var gutil        = require('gulp-util');
-var prefix       = require('gulp-autoprefixer');
 var rename       = require('gulp-rename');
 var plumber      = require('gulp-plumber');
 var sourcemaps   = require('gulp-sourcemaps');
