958e4a486853e6e3f89d21db9f741a81eb4a4b77
Dominic Gan <dominicgangx@gmail.com>
2018-04-06 00:21:25 +0800

    fix(css): update sass task to minify css
    - use minified css only
    - fix css task to include sourcemaps for minified version
    - update packages used

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,40 +1,26 @@
-var gulp 		= require('gulp');
-var sass 		= require('gulp-sass');
-var gutil 		= require('gulp-util');
-var prefix 		= require('gulp-autoprefixer');
-var rename 		= require('gulp-rename');
-var plumber 	= require('gulp-plumber');
-var cssnano 	= require('gulp-cssnano');
-var sourcemaps  = require('gulp-sourcemaps');
-var clip		= require('gulp-clip-empty-files');
-var clone		= require('gulp-clone');
-var merge		= require('merge-stream');
-var debug		= require('gulp-debug');
-var notify		= require('gulp-notify');
-
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var gutil        = require('gulp-util');
+var prefix       = require('gulp-autoprefixer');
+var rename       = require('gulp-rename');
+var plumber      = require('gulp-plumber');
+var sourcemaps   = require('gulp-sourcemaps');
+var clip         = require('gulp-clip-empty-files');
+var debug        = require('gulp-debug');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano      = require('cssnano');
 /**
  * Compile files from _scss into both _site/css (for live injecting)
  */
 var scss_src  = './assets/_scss/**/*.scss';
-var scss_dest = './dist/css';
+var scss_dest = './assets/_css';
+var css_dest = './dist/css';
 
 gulp.task('sass', function(){
-	var source = gulp.src(scss_src)
-	.pipe(plumber())
-	.pipe(sourcemaps.init()) // Start Sourcemaps
-	.pipe(sass({
-		// set output format for compiled css
-		outputStyle: 'expanded',
-		indentType: 'tab'
-		}).on('error', sass.logError))
-	.on('error', gutil.log);
-
-	var pipeMaps = source.pipe(clone())
-	.pipe(sourcemaps.write('maps'));
-
-	var pipeMinify = source.pipe(clone())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(cssnano({ // minify with cssnano
+	var plugins = [
+		autoprefixer({browsers: ['last 1 version']}),
+		cssnano({ // minify with cssnano
 		convertValues: {
 			length: false
 		},
@@ -46,10 +32,22 @@ gulp.task('sass', function(){
 			// (bug: cssnano completely deletes the rule instead of minifying)
 			fontFace: false
 		}
-	}))
-	.pipe(clip());
+	})
+	];
 
-	return merge(pipeMaps, pipeMinify)
-	.pipe(gulp.dest(scss_dest))
-	.pipe(debug({title: 'sass-merge:'}));
+	var source = gulp.src(scss_src)
+	.pipe(plumber())
+	.pipe(sourcemaps.init()) // Start Sourcemaps
+	.pipe(sass({
+		// set output format for compiled css
+		outputStyle: 'expanded',
+		indentType: 'tab'
+		}).on('error', sass.logError))
+	.on('error', gutil.log)
+	.pipe(postcss(plugins))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(sourcemaps.write('maps'))
+	.pipe(clip())
+	.pipe(gulp.dest(css_dest))
+	.pipe(debug({title: 'sass:'}))
 });
